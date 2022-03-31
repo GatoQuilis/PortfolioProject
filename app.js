2aaeb9dba3e208d7ff341bc1a80d674f99803a09
Franco Alejandro Diaz <franco.diaz@claro.com.ar>
2022-03-31 12:30:48 -0300

    agreando info al header

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions () {
     //Button click active class
@@ -9,9 +9,29 @@ function PageTransitions () {
         sectBtn[i].addEventListener ('click', function() {
             let currentBtn = document.querySelectorAll ('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn';
+            this.className += ' active-btn';
         })
     }
+
+    //Button click active class
+    allSections.addEventListener ('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            //remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active');
+
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
